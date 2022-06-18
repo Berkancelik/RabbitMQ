c9aafc2395e004cd91b081a9d715e391f1187b2c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-18 10:26:17 +0300

    Fanout Exchange

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -12,13 +12,15 @@ namespace RabbitMQ.publisher
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://dhqyhvke:5ukMea46fWrkSxEq53cPQxBqr9N92sEQ@moose.rmq.cloudamqp.com/dhqyhvke");
             using var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();           
-            channel.QueueDeclare("hello-queue", true, false, false);
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+            
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                string message = "hello world";
+                string message = $"log{x}";
                 var messageBody = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                channel.BasicPublish("logs-fanout"," ",null, messageBody);
                 Console.WriteLine("Mesaj başarılı şekilde gönderilmiştir");
 
             });
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -15,17 +15,22 @@ namespace RabbitMQ.subscriber
             factory.Uri = new Uri("amqps://dhqyhvke:5ukMea46fWrkSxEq53cPQxBqr9N92sEQ@moose.rmq.cloudamqp.com/dhqyhvke");
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            
+
+            var randomQueueName = "log-database-save";// channel.QueueDeclare().QueueName;
+            channel.QueueDeclare(randomQueueName, true, false, false);
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
             channel.BasicQos(0, 1, false);
             var consumer = new EventingBasicConsumer(channel);
-            channel.BasicConsume("hello-queue", false, consumer);
+            channel.BasicConsume(randomQueueName, false, consumer);
+            Console.WriteLine("Loglar dinleniyor..");
 
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
                 Thread.Sleep(1500);
-                Console.WriteLine("Gelen Mesaj" + message);
+                Console.WriteLine("Gelen Mesaj" + " "+ message);
                 channel.BasicAck(e.DeliveryTag, false);
             };
             Console.ReadLine();
